3dcf4ed83364634449fb9035e25eda801f7b7cc4
samhith123 <samhith.gardas123@gmail.com>
2018-04-19 00:16:31 +0530

    updated

--- app/src/main/java/com/example/satwik/blooddonor/main_screen.java
+++ app/src/main/java/com/example/satwik/blooddonor/main_screen.java
@@ -10,6 +10,7 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -29,12 +30,13 @@ public class main_screen extends Activity implements NavigationView.OnNavigation
     private Button log_out;
     private Button donate;
     private ProgressDialog progressDialog;
-    public DrawerLayout drawer;
+    public    DrawerLayout drawer;
+    private  ActionBarDrawerToggle toggle;
 
 
     protected void onCreate(Bundle savedInstanceStale) {
         super.onCreate(savedInstanceStale);
-        setContentView(R.layout.after_login);
+        setContentView(R.layout.activity_after_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         donate = (Button) findViewById(R.id.circle_btn_donate);
         log_out = (Button) findViewById(R.id.circle_logout);
@@ -66,15 +68,25 @@ public class main_screen extends Activity implements NavigationView.OnNavigation
                 finish();
             }
         });
-    }
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this); }
 
     @Override
     public void onBackPressed() {
-
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);}
+            else{
         super.onBackPressed();
         startActivity(new Intent(getBaseContext(), main_screen.class));
 
-    }
+    }}
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -102,6 +114,24 @@ public class main_screen extends Activity implements NavigationView.OnNavigation
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void onResume() {
